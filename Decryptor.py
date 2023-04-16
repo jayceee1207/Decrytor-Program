b62b04c340e38e9357d12582db3447736fd94f54
jayceee1207 <ablayjc7@gmail.com>
2023-04-16 09:35:19 +0800

    Pseudocode

---
+++ Decryptor.py
@@ -0,0 +1,13 @@
+#John Carlo Ablay
+#BSCPE 1-5
+#Code Encryptor
+
+#PSEUDOCODE
+#Ask the user for their input
+#Check each character of the string
+#   if character is *, change it to a
+#   if character is &, change it to e
+#   if character is #, change it to i
+#   if character is +, change it to o
+#   if character is !, change it to u
+#Print the resulting output
